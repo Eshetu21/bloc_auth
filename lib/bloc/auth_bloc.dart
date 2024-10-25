90232ce3e11b9269fd4a87d2ab68212dc93ec2ff
Eshetu21 <eshetutesema2050@gmail.com>
2024-10-25 17:30:04 +0300

    block auth

--- lib/bloc/auth_bloc.dart
+++ lib/bloc/auth_bloc.dart
@@ -5,31 +5,35 @@ part 'auth_state.dart';
 
 class AuthBloc extends Bloc<AuthEvent, AuthState> {
   AuthBloc() : super(AuthInitial()) {
-    on<AuthLoginRequested>((event, emit) async {
-      emit(AuthLoading());
-      try {
-        final email = event.email;
-        final password = event.password;
+    on<AuthLoginRequested>(_onAuthLoinRequested);
+    on<AuthLogoutRequested>(_onAuthLogoutRequested);
+  }
 
-        if (password.length < 6) {
-          return emit(AuthFailure("Password cannot be less than 6 characters"));
-        }
-        await Future.delayed(const Duration(seconds: 1), () {
-          return emit(AuthSucess("uid: $email - $password"));
-        });
-      } catch (e) {
-        return emit(AuthFailure(e.toString()));
+  void _onAuthLoinRequested(event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      final email = event.email;
+      final password = event.password;
+
+      if (password.length < 6) {
+        return emit(AuthFailure("Password cannot be less than 6 characters"));
       }
-    });
-    on<AuthLogoutRequested>((event, emit) async {
-      emit(AuthLoading());
-      try {
-        await Future.delayed(const Duration(seconds: 1), () {
-          return emit(AuthInitial());
-        });
-      } catch (e) {
-        emit(AuthFailure(e.toString()));
-      }
-    });
+      await Future.delayed(const Duration(seconds: 1), () {
+        return emit(AuthSucess("uid: $email - $password"));
+      });
+    } catch (e) {
+      return emit(AuthFailure(e.toString()));
+    }
+  }
+
+  void _onAuthLogoutRequested(event, Emitter<AuthState> emit) async {
+    emit(AuthLoading());
+    try {
+      await Future.delayed(const Duration(seconds: 1), () {
+        return emit(AuthInitial());
+      });
+    } catch (e) {
+      emit(AuthFailure(e.toString()));
+    }
   }
 }
